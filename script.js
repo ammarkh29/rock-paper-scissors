3b4017d7d88eb52aaa0de735694aad62447d8e52
bruhjabi <ammarahmedkhan29@gmail.com>
2025-05-15 17:48:42 -0400

    Implemented playRound and playGame
    Rock, Paper, Scissors is now fully functional. Game is played for 5 rounds and winner (or tie) is announced at the end.

--- script.js
+++ script.js
@@ -9,3 +9,64 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+
+
+function playGame() {  
+    let computerScore = 0;
+    let humanScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+    
+        if (humanChoice == "rock") {
+            if (computerChoice == "scissors") {
+                humanScore++;
+                return "You win! Rock beats scissors.";
+            } else if (computerChoice == humanChoice) {
+                return "Tie!";
+            } else {
+                computerScore++;
+                return "You lose! Paper beats rock.";
+            }
+        } else if (humanChoice == "paper") {
+            if (computerChoice == "rock") {
+                humanScore++;
+                return "You win! Paper beats rock.";
+            } else if (computerChoice == humanChoice) {
+                return "Tie!";
+            } else {
+                computerScore++;
+                return "You lose! Scissors beats paper.";
+            }
+        } else { // humanChoice must be scissors
+            if (computerChoice == "paper") {
+                humanScore++;
+                return "You win! Scissors beats paper.";
+            } else if (computerChoice == humanChoice) {
+                return "Tie!";
+            } else {
+                computerScore++;
+                return "You lose! Rock beats scissors.";
+            }
+        }
+    }
+
+    for (let index = 0; index < 5; index++) {
+        humanChoice = prompt("Choose rock, paper, or scissors: ")
+        console.log(playRound(humanChoice, getComputerChoice()));
+    }
+
+    if (humanScore > computerScore) {
+        console.log("You win!");
+    }
+    else if (humanScore == computerScore) {
+        console.log("Tie");
+    }
+    else {
+        console.log("Computer wins!");
+    }
+}
+
+
+
